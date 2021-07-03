e8421a6522641fe0b8016c6653950d8bc49cb772
Nazmul Alam <nazmuldipu@gmail.com>
2021-07-03 19:11:00 +0600

    dashboard grid added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: { },
+        extend: {
+            gridTemplateColumns: {
+                '2-min-fill': 'min-content 1fr'
+            },
+        },
     },
     variants: {
         extend: {},
